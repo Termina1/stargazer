0042fd6fddd0e6ca6a71c001b8c3693f16959691
Termina1 <terminal2010@gmail.com>
2014-07-01 20:12:31 +0400

    fix env

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ require "action_view/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
-puts ENV
+puts ENV.inspect
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
