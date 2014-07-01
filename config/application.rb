7c8448a5a313cbe85861d172ae9f3318f8c8232e
Termina1 <terminal2010@gmail.com>
2014-07-01 20:09:25 +0400

    fix

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ require "action_view/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
+puts ENV
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
