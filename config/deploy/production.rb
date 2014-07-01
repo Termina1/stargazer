d89d1284c213b905104e4889d8d0fae165456568
Termina1 <terminal2010@gmail.com>
2014-07-01 19:29:32 +0400

    change deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 # Simple Role Syntax
 # ==================
 set :rvm_ruby_version, '2.1@stargazer'
-server '188.226.219.153', user: 'terminal', roles: [:web, :app, :db]
+server '37.139.20.93', user: 'terminal', roles: [:web, :app, :db]
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_bind, "tcp://0.0.0.0:9999"    #accept array for multi-bind
@@ -13,29 +13,3 @@ set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
 set :puma_worker_timeout, nil
 set :puma_init_active_record, false
 set :puma_preload_app, true
-
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# And/or per server (overrides global)
-# ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
